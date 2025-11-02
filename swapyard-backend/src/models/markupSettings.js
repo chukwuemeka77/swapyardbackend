c2f54bda86fd5b249459a63ec9a8cf21391850f7
Chukwuemeka <101349109+chukwuemeka77@users.noreply.github.com>
2025-11-02 18:58:16 +0100

    Update markupSettings.js

--- swapyard-backend/src/models/markupSettings.js
+++ swapyard-backend/src/models/markupSettings.js
@@ -3,10 +3,14 @@ const mongoose = require("mongoose");
 const markupSettingSchema = new mongoose.Schema({
   type: {
     type: String,
-    enum: ["deposit", "exchange", "payment"],
+    enum: ["deposit", "exchange", "payment", "withdrawal"],
     required: true,
   },
-  percentage: { type: Number, required: true, min: 0 },
+  percentage: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
 });
 
 module.exports = mongoose.model("MarkupSetting", markupSettingSchema);
